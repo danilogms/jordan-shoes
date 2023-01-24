6de062c72da42b9d7db951034a90e7ca0758cd12
danilogms <Danmil-20@hotmail.com>
2023-01-24 13:00:19 -0300

    Header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/Header';
 import Shoes from './Components/Shoes';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Shoes />
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from '../Components/Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.promocao}>Ganhe R$ 10,00 de desconto no frete</div>
+      <div className={styles.title}>
+        <h2>JordanShoes</h2>
+      </div>
+      <div className={styles.logo}>
+        <h1 className="title">A melhor loja de Jordan</h1>
+        <p className={styles.paragrafo}>
+          O tênis Jordan é fruto de uma velha e forte parceria<br/>entre a
+          Nike e o jogador Michael Jordan.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
